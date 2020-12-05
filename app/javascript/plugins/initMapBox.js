4f11302cc61b079fc7678d8c5600f3f2360d4205
João Victor Ramires Guimarães Brito <joaoramires.engsoft@gmail.com>
2020-12-05 17:04:15 -0300

    Update initMapBox.js
    Refatorado para ficar mais fácil de entender o código e adiciona controles do mapbox: 
    padding (distância da borda do mapa até o elemento mais próximo),
    maxZoom: Distância Inicial do Mapa,
    Duration: Animação ao carregar o mapa.

--- app/javascript/plugins/initMapBox.js
+++ app/javascript/plugins/initMapBox.js
@@ -1,35 +1,57 @@
+import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
+// Constroí o Mapa
+const buildMap = (mapElement) => {
+  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+  return new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/joaoramires/cki9itu894klu19rw4oa2lwgc'
+  });
+};
+
+
+// Adiciona os Marcadores no Mapa
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+    const element = document.createElement('div');
+    element.className = 'marker';
+    element.style.backgroundImage = `url('${marker.image_url}')`;
+    element.style.backgroundSize = 'contain';
+    element.style.width = '25px';
+    element.style.height = '25px';
+
+    new mapboxgl.Marker(element)
+      .setLngLat([marker.lng, marker.lat])
+      .setPopup(popup)
+      .addTo(map);
+  });
+};
+
+// Define as Bordas do Mapa Utilizando os Elementos do nosso Banco de Dados
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1500 });
+};
+
+
+// Função que agrega tudo e chama as outras funções.
+// O addControl adiciona a pesquisa no mapa.
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-
-  if (mapElement) { // only build a map if there's a div#map to inject into
-    // Renderiza um mapa sem marcadores
-    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/joaoramires/cki9itu894klu19rw4oa2lwgc'
-    });
-    
-    // Adiciona a pesquisa no mapa
-    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl }));
-
-    // Insere os marcadores e Info Window no mapa
+  if (mapElement) {
+    const map = buildMap(mapElement);
     const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-      const element = document.createElement('div');
-      element.className = 'marker';
-      element.style.backgroundImage = `url('${marker.image_url}')`;
-      element.style.backgroundSize = 'contain';
-      element.style.width = '25px';
-      element.style.height = '25px';
-
-      new mapboxgl.Marker(element)
-        .setLngLat([ marker.lng, marker.lat ]).setPopup(popup).addTo(map);
-    });
+    addMarkersToMap(map, markers);
+    fitMapToMarkers(map, markers);
+    map.addControl(new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl
+    }));
   }
 };
 
